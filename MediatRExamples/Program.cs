82aa7bf3d6d2ae88111e56fb73e20ffed80c9c6b
oplatonov <osp.create@gmail.com>
2023-11-17 22:07:51 +0300

    Add MediatR NuGet package

--- MediatRExamples/Program.cs
+++ MediatRExamples/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddAuthorization(options =>
     options.FallbackPolicy = options.DefaultPolicy;
 });
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
